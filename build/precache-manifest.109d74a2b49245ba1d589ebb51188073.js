4372037e0ca8442b3af2997d5a5e8ad02914bebc
rishwan <a.rishwan@gmail.com>
2019-01-25 02:53:41 +0800

    included build dir

---
+++ build/precache-manifest.109d74a2b49245ba1d589ebb51188073.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbc0974073678f9cc97c647e18337438",
+    "url": "/static/media/octocat.dbc09740.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a196c128f2ab668075c7",
+    "url": "/static/js/main.a196c128.chunk.js"
+  },
+  {
+    "revision": "c2aa9b4fd10ecb5a4c99",
+    "url": "/static/js/1.c2aa9b4f.chunk.js"
+  },
+  {
+    "revision": "a196c128f2ab668075c7",
+    "url": "/static/css/main.3d22bced.chunk.css"
+  },
+  {
+    "revision": "c2aa9b4fd10ecb5a4c99",
+    "url": "/static/css/1.470c27ef.chunk.css"
+  },
+  {
+    "revision": "fcd8480ab5824443a45d6bcb0c353b26",
+    "url": "/index.html"
+  }
+];
